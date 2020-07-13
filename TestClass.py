a60b0a7e03394cbb4489eba57deb9a17b28f0ec0
hnnaa <hnnaa@sina.com>
2020-07-13 19:49:10 +0800

    类与对象

--- TestClass.py
+++ TestClass.py
@@ -1,9 +1,13 @@
 # encoding:utf-8
 import traceback
+from types import MethodType
 
 
 class Student(object):
-    __slots__ = ["name"]
+    _score = 0
+
+    # 限制属性
+    # __slots__ = ("name")
 
     def __enter__(self):
         return self
@@ -11,13 +15,46 @@ class Student(object):
     def __exit__(self, exc_type, exc_val, exc_tb):
         print(exc_type, exc_val, exc_tb)
 
+    # 构造
     def __init__(self, name):
         self.name = name
 
+    @property
+    def score(self):
+        return self._score
 
-s = Student("fdf")
+    @score.setter
+    def score(self, value):
+        if not isinstance(value, int):
+            raise ValueError("not int value")
+        if value < 0 or value > 100:
+            raise ValueError("value must between 0 and 100")
+        self._score = value
+
+
+def set_age(self, age):
+    self.age = age
+
+
+s = Student("lilei")
 print(s.name)
-# s.ff = ""
+print("name=" + getattr(s, "name"))
+# 绑定方法
+s.set_age = MethodType(set_age, s)
+s.set_age(18)
+print(s.age)
+# 设置属性@property
+print(s.score)
+try:
+    s.score = "s"
+except:
+    print(traceback.format_exc())
+try:
+    s.score = 1000
+except:
+    print(traceback.format_exc())
+s.score = 100
+print(s.score)
 try:
     with Student("cc") as ss:
         a = 0
--- test.py
+++ test.py
@@ -73,3 +73,4 @@ if __name__ == "__main__":
         i = i + 1
         if i > 10:
             break
+    
